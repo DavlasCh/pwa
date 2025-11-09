7a4516887bdec512519e92e39c9a5c9dcf82c6db
Nicolas <175035962+Nivan24@users.noreply.github.com>
2025-11-09 13:55:26 -0500

    Mostrar productos

--- app.js
+++ app.js
@@ -6,3 +6,26 @@ const productos = [
   { id: 5, nombre: "Soporte Magnético para Auto", precio: 30000, imagen: "https://cdn-icons-png.flaticon.com/512/3064/3064197.png" },
   { id: 6, nombre: "Funda Antigolpes", precio: 35000, imagen: "https://cdn-icons-png.flaticon.com/512/2740/2740598.png" }
 ];
+
+const productosDiv = document.getElementById("productos");
+const listaCarrito = document.getElementById("lista-carrito");
+const listaPedidos = document.getElementById("lista-pedidos");
+const totalP = document.getElementById("total");
+const btnComprar = document.getElementById("btn-comprar");
+
+let carrito = [];
+let pedidos = JSON.parse(localStorage.getItem("pedidos")) || [];
+
+function mostrarProductos() {
+  productos.forEach(p => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+      <img src="${p.imagen}" alt="${p.nombre}">
+      <h3>${p.nombre}</h3>
+      <p>$${p.precio.toLocaleString()}</p>
+      <button onclick="agregarAlCarrito(${p.id})">Agregar</button>
+    `;
+    productosDiv.appendChild(card);
+  });
+}
