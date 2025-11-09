e796b9c65c623f77108382f64ddc206e7962d303
Nicolas <175035962+Nivan24@users.noreply.github.com>
2025-11-09 13:42:40 -0500

    Se crean los productos

---
+++ app.js
@@ -0,0 +1,8 @@
+const productos = [
+  { id: 1, nombre: "Cargador Rápido USB-C", precio: 40000, imagen: "https://cdn-icons-png.flaticon.com/512/1053/1053184.png" },
+  { id: 2, nombre: "Cable Tipo C Reforzado", precio: 25000, imagen: "https://cdn-icons-png.flaticon.com/512/1040/1040227.png" },
+  { id: 3, nombre: "Audífonos Bluetooth", precio: 80000, imagen: "https://cdn-icons-png.flaticon.com/512/1046/1046433.png" },
+  { id: 4, nombre: "Protector de Pantalla", precio: 15000, imagen: "https://cdn-icons-png.flaticon.com/512/1164/1164954.png" },
+  { id: 5, nombre: "Soporte Magnético para Auto", precio: 30000, imagen: "https://cdn-icons-png.flaticon.com/512/3064/3064197.png" },
+  { id: 6, nombre: "Funda Antigolpes", precio: 35000, imagen: "https://cdn-icons-png.flaticon.com/512/2740/2740598.png" }
+];
